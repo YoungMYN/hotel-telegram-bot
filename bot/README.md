2b1995d40655e4dc3cc652dc34667be9a34b62b2
TommySnatch <89248355+YoungMYN@users.noreply.github.com>
2023-01-02 01:39:15 +0300

    Delete bot directory

--- bot/README.md
+++
@@ -1,2 +0,0 @@
-# hotel-telegram-bot
-A Telegram bot for a hotel that has the functions of booking a room, as well as displaying a calendar of prices, rooms, contact information, etc.
--- bot/src/main/java/young/myn/bot/languages/RU.java
+++
@@ -1,9 +0,0 @@
-package young.myn.bot.languages;
-
-public class RU {
-    public static final String START_STRING = "Hi, %s! Welcome to Holiday Hotel!\n" +
-            "This bot will help you book a room, show available dates, as well as our prices and much more.";
-    public static final String NOT_RECOGNISED_COMMAND = "Unfortunately, I don't understand the command.\n"+
-            " Please select an item from the menu.";
-
-}
