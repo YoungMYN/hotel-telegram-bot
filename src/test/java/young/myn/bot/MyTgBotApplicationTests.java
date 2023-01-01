4b01e8c9850f8cae92c3af5d3f16809f446d2b95
YoungMYN <Shyliko315on@gmail.com>
2023-01-02 01:47:54 +0300

    initial commit v.1

---
+++ src/test/java/young/myn/bot/MyTgBotApplicationTests.java
@@ -0,0 +1,13 @@
+package young.myn.bot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyTgBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
